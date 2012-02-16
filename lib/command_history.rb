92224ad822fc79968ddbfbe9e1cb626c22838adb
Kohei Tomita <tommy.fmale@gmail.com>
2012-02-16 14:12:29 +0900

    rafactoring: split function: CommandHistory#update_ellipsables

--- lib/command_history.rb
+++ lib/command_history.rb
@@ -7,25 +7,19 @@ class CommandHistory
   attr_accessor :ellipsis_count
   attr_accessor :ellipsables
   attr_accessor :alias_usages
+  attr_accessor :alias_list
 
   ZSH_HISTORY_FILE = "#{ENV["HOME"]}/.zsh_history"
 
   def initialize(commands, alias_list)
+    @alias_list = alias_list
     @commands = []
     @ellipsis_count = 0
     @ellipsables = Hash.new
     @alias_usages = Hash.new
 
     commands.each do |command|
-      if alias_list.ellipsable?(command)
-        if @ellipsables.has_key?(command)
-          ellipsable = @ellipsables[command]
-          ellipsable.count += 1
-        else
-          ellipsable = Ellipsable.new(command)
-          @ellipsables[command] = ellipsable
-        end
-      end
+      update_ellipsables(command)
       command_expanded = alias_list.expand_command(command)
       applied_alias = alias_list.applied_alias(command)
       applied_alias.each do |alias_, command|
@@ -65,4 +59,17 @@ class CommandHistory
     parameter, command = line.split(/;/)
     return command
   end
+
+  def update_ellipsables(command)
+    if alias_list.ellipsable?(command)
+      if @ellipsables.has_key?(command)
+        ellipsable = @ellipsables[command]
+        ellipsable.count += 1
+      else
+        ellipsable = Ellipsable.new(command)
+        @ellipsables[command] = ellipsable
+      end
+    end
+  end
+
 end
