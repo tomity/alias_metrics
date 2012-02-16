51a9863eb3a9f0d76f252af3eadc8334dc44ae9f
Kohei Tomita <tommy.fmale@gmail.com>
2012-02-16 17:02:26 +0900

    add spec: when alias apply a part of command

--- spec/alias_list_spec.rb
+++ spec/alias_list_spec.rb
@@ -6,6 +6,11 @@ describe AliasList do
     alias_list.expand_command("l").should == "ls -la"
   end
 
+  it "should output `ls -la -h` for the command `l -h` " do
+    alias_list = AliasList.load_from_lines(["l='ls -la'"])
+    alias_list.expand_command("l -h").should == "ls -la -h"
+  end
+
   it "should output `ls` for the command `sl` " do
     alias_list = AliasList.load_from_lines(["sl=ls"])
     alias_list.expand_command("sl").should == "ls"
@@ -24,6 +29,11 @@ describe AliasList do
   it "should output `ls -la` is shortenable" do
     alias_list = AliasList.load_from_lines(["l='ls -la'"])
     alias_list.shortenable?("ls -la").should == true
+  end
+
+  it "should output `ls -la -h` for the command `l -h` " do
+    alias_list = AliasList.load_from_lines(["l='ls -la'"])
+    alias_list.shortenable?("ls -la -h").should == true
   end
 
   it "should output `ls -l` is not shortenable" do
